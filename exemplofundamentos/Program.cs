4d2e0340fc550180e356c83d5ead5f80391069fa
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-22 07:26:48 -0300

    BootCamp DIO .Net

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -6,19 +6,22 @@ using exemplofundamentos.Models;
 string apresentacao = "Olá, seja bem vindo";
 
 int quantidade = 1;
-
-double altura = 1.5;
-
-decimal preco = 1.80M;
-
-bool condicao = true;
-
-
-Console.WriteLine(apresentacao);
 Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0,00"));
-Console.WriteLine("Valor da variável preco: " + preco);
-Console.WriteLine("Valor da variável condição: " + condicao);
+
+quantidade = 10;
+
+// double altura = 1.5;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0,00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condição: " + condicao);
 
 
 // Pessoa pessoa1 = new Pessoa();
