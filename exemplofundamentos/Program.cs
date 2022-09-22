d7f22cbca71460daf442015913d775360f8a6032
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-22 07:45:44 -0300

    BootCamp DIO .Net

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -1,15 +1,18 @@
 ﻿//exemplofundamentos.Models.Pessoa p = new exemplofundamentos.Models.Pessoa();
 
-using exemplofundamentos.Models;
+ using exemplofundamentos.Models;
+
+ DateTime dataAtual = DateTime.Now.AddDays(5);
+ Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
 
-string apresentacao = "Olá, seja bem vindo";
+// string apresentacao = "Olá, seja bem vindo";
 
-int quantidade = 1;
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
-quantidade = 10;
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
 
 // double altura = 1.5;
