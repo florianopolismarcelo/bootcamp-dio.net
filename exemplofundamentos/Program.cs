9471c18c0ce5c925a163c0916caf11734fdefd6e
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-26 15:54:46 -0300

    BootCamp DIO .Net

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -5,8 +5,14 @@ using exemplofundamentos.Models;
 
 int quantidadeEmEstoque = 10;
 int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-if  (quantidadeEmEstoque >= quantidadeCompra){
+    Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+    Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+    Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+
+if  (possivelVenda){
 
     Console.WriteLine("Venda realizada");
 }
