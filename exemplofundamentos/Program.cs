a80e75ad56a7534b62ba1347330b16ba728112e9
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-27 15:29:14 -0300

    BootCamp DIO .Net

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -3,7 +3,17 @@
 using exemplofundamentos.Models;
 
 
+bool choveu = true;
+bool estaTarde = false;
 
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia");
+}
 
 
 // bool possuPresencaMinima = false;
