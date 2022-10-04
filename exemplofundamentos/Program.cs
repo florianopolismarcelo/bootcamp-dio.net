1560cdf0b23d9986fa7c6cf79f372c986494ac6e
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-04 14:47:12 -0300

    BootCamp DIO .Net

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -3,11 +3,22 @@
 using exemplofundamentos.Models;
 
 int numero = 5;
+int contador = 0;
 
-for(int contador = 0; contador <=10; contador++)
+while (contador <= 10)
 {
-Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+Console.WriteLine($"{contador + 1}º Execução: {numero} x {contador} = {numero * contador}");
+contador++;
 }
+
+
+
+// int numero = 5;
+
+// for(int contador = 0; contador <=10; contador++)
+// {
+// Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
 
 
 
