5801d744acfad8021f23a259d278231c203410ea
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-10 14:43:49 -0300

    BootCamp DIO .Net

--- Rascunho/Program.cs
+++ Rascunho/Program.cs
@@ -4,7 +4,18 @@ arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
+Console.WriteLine("Percorrendo o Array com o FOR");
+
 for (int contador = 0; contador < arrayInteiros.Length; contador++)
 {
-   //Console.WriteLine($"Posição N° {contador} = {arrayInteiros[contador]}");
+    Console.WriteLine($"Posição N° {contador} = {arrayInteiros[contador]}");
+}
+Console.WriteLine("");
+Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+int contadorForeach = 0;
+foreach (int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} = {valor}");
+    contadorForeach++;
 }
