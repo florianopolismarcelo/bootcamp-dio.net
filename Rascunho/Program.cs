ffc6a2983b3df3fcbf207794a8e5214b41a75e05
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-10 16:32:30 -0300

    BootCamp DIO .Net

--- Rascunho/Program.cs
+++ Rascunho/Program.cs
@@ -1,20 +1,35 @@
-﻿int[] arrayInteiros = new int[4];
-
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
+﻿
 
 
-int[] arrayInteiroDobrado = new int[arrayInteiros.Length * 2];
-Array.Copy(arrayInteiros, arrayInteiroDobrado, arrayInteiros.Length);
 
-//Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2 );
 
-Console.WriteLine("Percorrendo o Array com o FOR");
-for (int contador = 0; contador < arrayInteiros.Length; contador++)
-{
-    Console.WriteLine($"Posição N° {contador} = {arrayInteiros[contador]}");
-}
+
+
+
+
+
+
+
+
+
+
+// int[] arrayInteiros = new int[4];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+
+
+// int[] arrayInteiroDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteiroDobrado, arrayInteiros.Length);
+
+// //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2 );
+
+// Console.WriteLine("Percorrendo o Array com o FOR");
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição N° {contador} = {arrayInteiros[contador]}");
+// }
 
 
 // Console.WriteLine("");
