c5da1d0936b4555979ef2ed5afebe90e40c2ece5
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-13 16:23:49 -0300

    BootCamp DIO .Net

--- trilha-net-fundamentos-desafio/obj/Debug/net6.0/trilha-net-fundamentos-desafio.AssemblyInfo.cs
+++ trilha-net-fundamentos-desafio/obj/Debug/net6.0/trilha-net-fundamentos-desafio.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("exemplofundamentos")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("trilha-net-fundamentos-desafio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("exemplofundamentos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("exemplofundamentos")]
+[assembly: System.Reflection.AssemblyProductAttribute("trilha-net-fundamentos-desafio")]
+[assembly: System.Reflection.AssemblyTitleAttribute("trilha-net-fundamentos-desafio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
